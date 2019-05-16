6e38b37dafe6c715573d4003bdb5199aa2033564
wangjing <4464291@qq.com>
2019-05-16 19:49:54 +0800

    添加一个函数

--- src/main/java/com/ustc/App.java
+++ src/main/java/com/ustc/App.java
@@ -6,8 +6,12 @@ package com.ustc;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+    }
+
+
+    public static int add(int a,int b){
+        return a + b;
     }
 }
