5a20339b68f032d231e8cdbdd4cf21a8432dac5d
wangjing <4464291@qq.com>
2019-06-05 10:16:48 +0800

    “加入乘法和除法”

--- src/main/java/com/ustc/App.java
+++ src/main/java/com/ustc/App.java
@@ -15,4 +15,12 @@ public class App
     public static int add(int a,int b){
         return a + b;
     }
+
+    public static int multiply(int a,int b){
+        return a * b;
+    }
+
+    public static int dic(int a,int b){
+        return a / b;
+    }
 }
